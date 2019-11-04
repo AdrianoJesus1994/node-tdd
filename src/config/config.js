c2c204c79051d10fde9deac047785f5ab6bcf255
Adriano da Silva de Jesus <adriano.jesus@capgemini.com.br>
2019-11-04 17:18:44 -0300

    correção de erro de build

---
+++ src/config/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "Meire2107",
+    database: "node_tdd",
+    host: "127.0.0.1",
+    port: "5432",
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+    }
+  },
+  test: {
+    sername: "postgres",
+    password: "Meire2107",
+    database: "node_tdd",
+    host: "127.0.0.1",
+    port: "5432",
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+    }
+  },
+  production: {
+    sername: "postgres",
+    password: "Meire2107",
+    database: "node_tdd",
+    host: "127.0.0.1",
+    port: "5432",
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+    }
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,13 @@
 const route = require("express").Router();
 
-module.exports = route();
+console.log("teste");
+
+// const { User } = require("./app/models");
+
+// User.create({
+//   name: "Adriano da Silv de Jesus",
+//   email: "asjesus1994@gmail.com",
+//   password_hash: "sdda4sdf4as4dfa54s46g2ag49a8494f4awdf"
+// });
+
+module.exports = route;
