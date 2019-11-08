41f11497d3a50a84504b8e421215e50480e2bf7a
Adriano da Silva de Jesus <adriano.jesus@capgemini.com.br>
2019-11-08 17:28:18 -0300

    criação do test de criação de usuário

--- __tests__/integration/auth.test.js
+++ __tests__/integration/auth.test.js
@@ -1,16 +1,32 @@
 const { User } = require("../../src/app/models");
+const truncate = require("./../Utils/truncate");
+const request = require("supertest");
+const app = require("../../src/app");
 
 describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it("should crete user when receive user data", async () => {
-    const user = await User.create({
-      name: "Alessandro da Silva de Jesus",
-      email: "asjesus1999@gmail.com",
-      password_hash: "sdda4sdf4as4dfa54s46g2ag49a8494f4awdf"
-    });
+    // const user = await User.create({
+    //   name: "Alessandro da Silva de Jesus",
+    //   email: "asjesus1999@gmail.com",
+    //   password_hash: "sdda4sdf4as4dfa54s46g2ag49a8494f4awdf"
+    // });
 
-    console.log(user);
+    // console.log(user);
 
-    expect(user.email).toBe("asjesus1999@gmail.com");
+    return request(app)
+      .post("/create-user")
+      .send({
+        name: "Alessandro da Silva de Jesus",
+        email: "asjesus1999@gmail.com",
+        password_hash: "sdda4sdf4as4dfa54s46g2ag49a8494f4awdf"
+      })
+      .expect(200, {});
+
+    // expect(request.status).toBe(200);
   });
 
   it("should receive JWT token when authenticated with valid credentials", () => {
